2d836a422784935d77f7cba33b85f7a0e9f13abe
Dasom <ektha5032@gmail.com>
2026-03-20 00:45:17 +0900

    :art: design-system style: mobile header, menu components 제작

---
+++ src/components/MobHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { PhBell } from "@phosphor-icons/vue";
+</script>
+
+<template>
+    <header>
+        <div class="header-section">
+            <div class="wrap">
+                <div class="logo-box">
+                    <!-- 추후에 a태그 연결 -->
+                    <p class="logo">ReMine</p>
+                </div>
+                <div class="icon-box">
+                    <button class='icon header-notice' type='button' aria-label='알림'>
+                        <PhBell :size='24'></PhBell>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style>
+.header-section {
+    position: sticky;
+    width: 100%;
+    height: 7rem;
+    top: 0;
+    box-shadow: 0px 2px 6px #0000001e;
+    z-index: 999;
+}
+
+.header-section .wrap {
+    display: flex;
+    height: inherit;
+    align-items: center;
+    justify-content: space-between;
+    background: var(--bg-elevated);
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./assets/styles/reset.css";
+import "./assets/styles/tokens.css";
+import "./assets/styles/common.css";
 
 createApp(App).use(router).mount("#app");
