98e4dc20960c574dce67e2790ad38f8a26f54bc8
Dasom <ektha5032@gmail.com>
2026-03-23 22:39:03 +0900

    :recycle: design-system refactor: 기존 홈 화면에 있던 mob header mob menu 컴포넌트 삭제

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import MobHeader from "../components/MobHeader.vue";
-import MobMenu from "../components/MobMenu.vue";
 </script>
 
 <template>
-    <MobHeader />
     <section class="home-section">
         <div class="wrap">
             로그인 후 홈 화면
         </div>
     </section>
-    <MobMenu />
 </template>
