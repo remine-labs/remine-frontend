c1945b3e10dd77352672364d2b1b7bdd3babcac0
Dasom <ektha5032@gmail.com>
2026-07-20 00:07:41 +0900

    :art: ui-polich style: 반응형 및 폴더 구조화

--- src/views/profile/Watchlist.vue
+++ src/views/profile/Watchlist.vue
@@ -6,7 +6,6 @@ import { getWatchlist, deleteWatchlist, addWatchlist } from '../../api/watchlist
 import type { Watchlist } from '../../api/watchlist.ts';
 
 const router = useRouter();
-
 const watchlist = ref<Watchlist[]>([]);
 
 const goToPlaylists = () => {
